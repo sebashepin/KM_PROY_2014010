f40e6a1fa2a979c9d1c741a554e7736d4d1a6e34
Daniel Salamanca <dsdesign@danielsalamanca.com>
2014-10-31 06:32:00 -0500

    frontend v01
    Subida de archivos del frontEnd en html, css, jquery y jqueryMobile

---
+++ howToBogota/js/createSteps.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+ 
+    var counter = 2;
+ 
+    $("#crearPaso").click(function () {
+ 
+	if(counter>10){
+            alert("Only 10 textboxes allow");
+            return false;
+	}   
+ 
+	var divStep = $(document.createElement('div'))
+	     .attr("id", 'divStep' + counter);
+ 
+	divStep.after().html('<label><span class="textoPAso">STEP</span><span class="numeroPaso">'+ counter + ' </span></label>' +
+	      '<input type="text" name="step' + counter + 
+	      '" id="step' + counter + '" value="" >');
+ 
+	divStep.appendTo("#pasosPregunta");
+ 
+ 
+	counter++;
+     });
+ 
+     $("#borrarPaso").click(function () {
+	if(counter==2){
+          alert("We need at least 1 step");
+          return false;
+       }   
+ 
+	counter--;
+ 
+        $("#divStep" + counter).remove();
+ 
+     });
+ 
+    
+  });
