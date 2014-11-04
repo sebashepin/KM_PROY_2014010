2bec013efaf00df678dd8f3e10fc55375219ce8b
Daniel Salamanca <dsdesign@danielsalamanca.com>
2014-11-03 20:02:10 -0500

    ya hace loop entre los dos jsons

--- howToBogota/js/callJSON.js
+++ howToBogota/js/callJSON.js
@@ -15,27 +15,9 @@ $(document).ready(function() {
 				$('#respuesta .contenedor #contenidoPrincipal').append('<div id="lasrespuestas"><div class="swiper-container"><div class="swiper-wrapper"><div class="swiper-slide">');
 				  	$('.swiper-slide').append('<p class="paso"><span class="textoPAso">STEP</span> <span class="numeroPaso">' + respuesta.step.step_id+ '</span></p>');
              		$('.swiper-slide').append('<p class="stepAnswer"> Description: ' + respuesta.step.step_description+ ' y mis vecinos son ' + respuesta.step.steps_neighborhood + '</p>');
-					
-					if(respuesta.step.steps_neighborhood.charAt(0) == "1" ){ 
-						$('.swiper-slide').append('<a href"#" class="arriba"></a>');
-						
-					}
-					if(respuesta.step.steps_neighborhood.charAt(1) == "1" ){ 
-						$('.swiper-slide').append('<a href"#" class="derecha"></a>');
-									
-					}
-					if(respuesta.step.steps_neighborhood.charAt(2) == "1" ){ 
-						$('.swiper-slide').append('<a href"#" class="abajo"></a>');
-												
-					}
-					if(respuesta.step.steps_neighborhood.charAt(3) == "1" ){ 
-						$('.swiper-slide').append('<a href"#" class="izquierda"></a>');
-												
-					}
-					llamarSiguientePaso();
-					
+					crearBotones(1);
 				$('#respuesta .contenedor #contenidoPrincipal').append('</div></div></div></div');
-            
+            	 
 			  }
 			  else if(respuesta.status == "NEW"){
 				  $('#respuesta .contenedor #contenidoPrincipal').append('<div id="lasrespuestas"><p>There are not answers for this Question.</p>'+'<p>Would you like to answer it?</p>'+'<div id="botonera"><a href="#responderPregunta" class="botonPreguntaGrande alLado">YES</a> <a href="#preguntar" class="botonPreguntaGrande alLado">NO</a></div></div> ');
@@ -43,47 +25,168 @@ $(document).ready(function() {
 			  else if(respuesta.status == "INVALID"){
 				  $('#respuesta .contenedor #contenidoPrincipal').append('<div id="lasrespuestas"><p>Your question is invalid. Please go back and ask again</p>'+'<a href="#preguntar" class="botonPreguntaGrande alLado">GO BACK</a></div>');				
 			  }
+			  
+			  
           });
 		  
 	
-
+var crearBotones = function(setBotones){
+	if(setBotones==1){
+				 $.getJSON('js/answer3.json', function(respuesta) {		
+						if(respuesta.step.steps_neighborhood.charAt(0) == "1" ){ 
+							$('.swiper-slide').append('<a href"#" class="arriba"></a>');	
+						}
+						if(respuesta.step.steps_neighborhood.charAt(1) == "1" ){ 
+							$('.swiper-slide').append('<a href"#" class="derecha"></a>');
+										
+						}
+						if(respuesta.step.steps_neighborhood.charAt(2) == "1" ){ 
+							$('.swiper-slide').append('<a href"#" class="abajo"></a>');
+													
+						}
+						if(respuesta.step.steps_neighborhood.charAt(3) == "1" ){ 
+							$('.swiper-slide').append('<a href"#" class="izquierda"></a>');
+													
+						} 
+						llamarSiguientePaso(1);
+						
+				 });
+	}
+	if(setBotones==2){
+		$.getJSON('js/nextStep.json', function(respuesta) {		
+						if(respuesta.step.steps_neighborhood.charAt(0) == "1" ){ 
+							$('.swiper-slide').append('<a href"#" class="arriba"></a>');	
+						}
+						if(respuesta.step.steps_neighborhood.charAt(1) == "1" ){ 
+							$('.swiper-slide').append('<a href"#" class="derecha"></a>');
+										
+						}
+						if(respuesta.step.steps_neighborhood.charAt(2) == "1" ){ 
+							$('.swiper-slide').append('<a href"#" class="abajo"></a>');
+													
+						}
+						if(respuesta.step.steps_neighborhood.charAt(3) == "1" ){ 
+							$('.swiper-slide').append('<a href"#" class="izquierda"></a>');
+													
+						}
+						llamarSiguientePaso(2);
+						
+				 });
+		
+	}
+				
+			  }
 //primerPaso();
 		  
-var llamarSiguientePaso= function(){
+var llamarSiguientePaso= function(pasoActual){
 	var botones = ['.arriba','.derecha','.abajo','.izquierda'];
 	var direccion = [
 			{bottom:'-300px',opacity:'0'},
 			{left:'-300px',opacity:'0'},
 			{top:'-300px',opacity:'0'},
 			{right:'-300px',opacity:'0'}];
-	
-	for(var i=0; i<=3;i++){
-		
-		console.log(botones[i]);
-		
-	$(botones[i]).click(function(){
-		
-							$.getJSON('js/nextStep.json', function(respuesta) {
-								console.log(i);
-								
-								$('.swiper-slide').animate(direccion[i-1],500);
-					
-								
+			
+	var velocidad = 500;
+	if(pasoActual==1){
+	$.getJSON('js/nextStep.json', function(respuesta) {
+	$('.arriba').click(function(){		
+							
+														
+								$('.swiper-slide').animate({bottom:'-300px',opacity:'0'},(velocidad-100));
 								 $('.swiper-slide').html('<p class="paso"><span class="textoPAso">STEP</span> <span class="numeroPaso">' + respuesta.step.step_id+ '</span></p>');
              		$('.swiper-slide').append('<p class="stepAnswer"> Description: ' + respuesta.step.step_description+ ' y mis vecinos son ' + respuesta.step.steps_neighborhood + '</p>');
-				
-					
-								
-								//$('.swiper-slide').animate({left:'300px'},0);
-								//$('.swiper-slide').animate({left:'0px',opacity:'1'},500);
-								
-					
-							});
+								$('.swiper-slide').animate({bottom:'300px'},0);
+								$('.swiper-slide').animate({bottom:'0px',opacity:'1'},velocidad);
+								crearBotones(2);
+						
+						});
+						
+	$('.derecha').click(function(){		
 							
+														
+								$('.swiper-slide').animate({left:'-300px',opacity:'0'},(velocidad-100));
+								 $('.swiper-slide').html('<p class="paso"><span class="textoPAso">STEP</span> <span class="numeroPaso">' + respuesta.step.step_id+ '</span></p>');
+             		$('.swiper-slide').append('<p class="stepAnswer"> Description: ' + respuesta.step.step_description+ ' y mis vecinos son ' + respuesta.step.steps_neighborhood + '</p>');
+								crearBotones(2);
+								$('.swiper-slide').animate({left:'300px'},0);
+								$('.swiper-slide').animate({left:'0px',opacity:'1'},velocidad);
 							
 						});
+	$('.abajo').click(function(){		
+							
+														
+								$('.swiper-slide').animate({top:'-300px',opacity:'0'},(velocidad-100));
+								 $('.swiper-slide').html('<p class="paso"><span class="textoPAso">STEP</span> <span class="numeroPaso">' + respuesta.step.step_id+ '</span></p>');
+             		$('.swiper-slide').append('<p class="stepAnswer"> Description: ' + respuesta.step.step_description+ ' y mis vecinos son ' + respuesta.step.steps_neighborhood + '</p>');
+								$('.swiper-slide').animate({top:'300px'},0);
+								$('.swiper-slide').animate({top:'0px',opacity:'1'},velocidad);
+								crearBotones(2);
+						
+						});
+	$('.izquierda').click(function(){		
+						
+								$('.swiper-slide').animate({right:'300px',opacity:'0'},(velocidad-100));
+								 $('.swiper-slide').html('<p class="paso"><span class="textoPAso">STEP</span> <span class="numeroPaso">' + respuesta.step.step_id+ '</span></p>');
+             		$('.swiper-slide').append('<p class="stepAnswer"> Description: ' + respuesta.step.step_description+ ' y mis vecinos son ' + respuesta.step.steps_neighborhood + '</p>');
+								$('.swiper-slide').animate({right:'-300px'},0);
+								$('.swiper-slide').animate({right:'0px',opacity:'1'},velocidad);
+								crearBotones(2);
+						
+						});
+						
+	});
 	}
 	
+	if(pasoActual==2){
+	$.getJSON('js/answer3.json', function(respuesta) {
+	$('.arriba').click(function(){		
+							
+														
+								$('.swiper-slide').animate({bottom:'-300px',opacity:'0'},(velocidad-100));
+								 $('.swiper-slide').html('<p class="paso"><span class="textoPAso">STEP</span> <span class="numeroPaso">' + respuesta.step.step_id+ '</span></p>');
+             		$('.swiper-slide').append('<p class="stepAnswer"> Description: ' + respuesta.step.step_description+ ' y mis vecinos son ' + respuesta.step.steps_neighborhood + '</p>');
+								$('.swiper-slide').animate({bottom:'300px'},0);
+								$('.swiper-slide').animate({bottom:'0px',opacity:'1'},velocidad);
+								crearBotones(1);
+						
+						});
+						
+	$('.derecha').click(function(){		
+							
+														
+								$('.swiper-slide').animate({left:'-300px',opacity:'0'},(velocidad-100));
+								 $('.swiper-slide').html('<p class="paso"><span class="textoPAso">STEP</span> <span class="numeroPaso">' + respuesta.step.step_id+ '</span></p>');
+             		$('.swiper-slide').append('<p class="stepAnswer"> Description: ' + respuesta.step.step_description+ ' y mis vecinos son ' + respuesta.step.steps_neighborhood + '</p>');
+								crearBotones(1);
+								$('.swiper-slide').animate({left:'300px'},0);
+								$('.swiper-slide').animate({left:'0px',opacity:'1'},velocidad);
+							
+						});
+	$('.abajo').click(function(){		
+							
+														
+								$('.swiper-slide').animate({top:'-300px',opacity:'0'},(velocidad-100));
+								 $('.swiper-slide').html('<p class="paso"><span class="textoPAso">STEP</span> <span class="numeroPaso">' + respuesta.step.step_id+ '</span></p>');
+             		$('.swiper-slide').append('<p class="stepAnswer"> Description: ' + respuesta.step.step_description+ ' y mis vecinos son ' + respuesta.step.steps_neighborhood + '</p>');
+								$('.swiper-slide').animate({top:'300px'},0);
+								$('.swiper-slide').animate({top:'0px',opacity:'1'},velocidad);
+								crearBotones(1);
+						
+						});
+	$('.izquierda').click(function(){		
+						
+								$('.swiper-slide').animate({right:'300px',opacity:'0'},(velocidad-100));
+								 $('.swiper-slide').html('<p class="paso"><span class="textoPAso">STEP</span> <span class="numeroPaso">' + respuesta.step.step_id+ '</span></p>');
+             		$('.swiper-slide').append('<p class="stepAnswer"> Description: ' + respuesta.step.step_description+ ' y mis vecinos son ' + respuesta.step.steps_neighborhood + '</p>');
+								$('.swiper-slide').animate({right:'-300px'},0);
+								$('.swiper-slide').animate({right:'0px',opacity:'1'},velocidad);
+								crearBotones(1);
+						
+						});
+						
+	});
+	}
+		
 }
 		  
 		 
