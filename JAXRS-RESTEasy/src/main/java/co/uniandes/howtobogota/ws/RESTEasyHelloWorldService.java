0862d83033791366c2c72148ce19ec9ff06eae95
Sebastian <sebashepin@gmail.com>
2014-11-03 20:02:54 -0500

    Add WS method implementations

--- JAXRS-RESTEasy/src/main/java/co/uniandes/howtobogota/ws/RESTEasyHelloWorldService.java
+++ JAXRS-RESTEasy/src/main/java/co/uniandes/howtobogota/ws/RESTEasyHelloWorldService.java
@@ -19,84 +19,88 @@ import co.uniandes.howtobogota.engine.KnowledgeEngine.STEP_NEIGHBOR;
 @Path("/HowToBogota")
 public class RESTEasyHelloWorldService {
 
-	@GET
-	@Path("/test/{pathParameter}")
-	@Produces("application/json")
-	public Response responseMsg( @PathParam("pathParameter") String pathParameter,
-			@DefaultValue("Nothing to say") @QueryParam("queryParameter") String queryParameter) {
+  @GET
+  @Path("/test/{pathParameter}")
+  @Produces("application/json")
+  public Response responseMsg(@PathParam("pathParameter") String pathParameter,
+      @DefaultValue("Nothing to say") @QueryParam("queryParameter") String queryParameter) {
 
-		String response = "Hello from: " + pathParameter + " : " + queryParameter;
+    String response = "Hello from: " + pathParameter + " : " + queryParameter;
 
-		return Response.status(200).entity(response).build();
-	}
-	
-	@GET
-	@Path("/answer")
-	@Produces("application/json")
-	public AnswerResponse placeholderAnswer() {
-		AnswerResponse st = new AnswerResponse(AnswerResponse.STATUS.OK, new Answer("23", "I'm the first and most popular step", "1101"));
+    return Response.status(200).entity(response).build();
+  }
 
-		return st;
-	}
-	
-	@POST
-	@Path("/GetAnswer")
-	@Produces("application/json")
-	public Object answerQuestion(@FormParam("question") String question) {
-		
-		return null;
-	}
-	
+  @GET
+  @Path("/answer")
+  @Produces("application/json")
+  public AnswerResponse placeholderAnswer() {
+    AnswerResponse st =
+        new AnswerResponse(AnswerResponse.STATUS.OK, new Answer("23",
+            "I'm the first and most popular step", "1101"));
 
-	@POST
-	@Path("/GetStep")
-	@Produces("application/json")
-	public AnswerResponse getStep(@FormParam("stepId") String stepId, @FormParam("step_direction") String stepDirectionString ) {
-		STEP_NEIGHBOR relativeDirection = null;
-		int direction = Integer.parseInt(stepDirectionString);
-		
-		switch(direction){
-			case 1000:
-				relativeDirection = STEP_NEIGHBOR.UP;
-				break;
-			case 0100:
-				relativeDirection = STEP_NEIGHBOR.NEXT;
-				break;
-			case 0010:
-				relativeDirection = STEP_NEIGHBOR.DOWN;
-				break;
-			case 0001:
-				relativeDirection = STEP_NEIGHBOR.PREVIOUS;
-				break;
-			default:
-				relativeDirection = STEP_NEIGHBOR.NEXT;
-		}
-		
-		KnowledgeEngine knowledgeEngine = KES.getInstance().getKnowledgeEngine();
-		return knowledgeEngine.getStepNeighbor(stepId, relativeDirection);
-	}
-	
-	@POST
-	@Path("/CreateQuestion")
-	@Produces("application/json")
-	public boolean createQuestion(@FormParam("question") String question) {
-		KnowledgeEngine knowledgeEngine = KES.getInstance().getKnowledgeEngine();
-		return knowledgeEngine.createQuestion(question);
-	}
-	
-	@POST
-	@Path("/AddFirstStep")
-	@Produces("application/json")
-	public String addFirstStep(@FormParam("question") String question, @FormParam("step_description") String stepDescription ) {
-		KnowledgeEngine knowledgeEngine = KES.getInstance().getKnowledgeEngine();
-		return knowledgeEngine.addFirstStep(question, stepDescription);
-	}
-	
-	@POST
-	@Path("/AddFirstStep")
-	@Produces("application/json")
-	public String addStep(@FormParam("previous_step_id") String previousStepId, @FormParam("step_description") String stepDescription ) {
-		KnowledgeEngine knowledgeEngine = KES.getInstance().getKnowledgeEngine();
-		return knowledgeEngine.addStep(previousStepId, null, stepDescription);
-	}	
+    return st;
+  }
+
+  @POST
+  @Path("/GetAnswer")
+  @Produces("application/json")
+  public Object answerQuestion(@FormParam("question") String question) {
+
+    return null;
+  }
+
+  @POST
+  @Path("/GetStep")
+  @Produces("application/json")
+  public AnswerResponse getStep(@FormParam("stepId") String stepId,
+      @FormParam("step_direction") String stepDirectionString) {
+    STEP_NEIGHBOR relativeDirection = null;
+    int direction = Integer.parseInt(stepDirectionString);
+
+    switch (direction) {
+      case 1000:
+        relativeDirection = STEP_NEIGHBOR.UP;
+        break;
+      case 0100:
+        relativeDirection = STEP_NEIGHBOR.NEXT;
+        break;
+      case 0010:
+        relativeDirection = STEP_NEIGHBOR.DOWN;
+        break;
+      case 0001:
+        relativeDirection = STEP_NEIGHBOR.PREVIOUS;
+        break;
+      default:
+        relativeDirection = STEP_NEIGHBOR.NEXT;
+    }
+
+    KnowledgeEngine knowledgeEngine = KES.getInstance().getKnowledgeEngine();
+    return knowledgeEngine.getStepNeighbor(stepId, relativeDirection);
+  }
+
+  @POST
+  @Path("/CreateQuestion")
+  @Produces("application/json")
+  public boolean createQuestion(@FormParam("question") String question) {
+    KnowledgeEngine knowledgeEngine = KES.getInstance().getKnowledgeEngine();
+    return knowledgeEngine.createQuestion(question);
+  }
+
+  @POST
+  @Path("/AddFirstStep")
+  @Produces("application/json")
+  public String addFirstStep(@FormParam("question") String question,
+      @FormParam("step_description") String stepDescription) {
+    KnowledgeEngine knowledgeEngine = KES.getInstance().getKnowledgeEngine();
+    return knowledgeEngine.addFirstStep(question, stepDescription);
+  }
+
+  @POST
+  @Path("/AddFirstStep")
+  @Produces("application/json")
+  public String addStep(@FormParam("previous_step_id") String previousStepId,
+      @FormParam("step_description") String stepDescription) {
+    KnowledgeEngine knowledgeEngine = KES.getInstance().getKnowledgeEngine();
+    return knowledgeEngine.addStep(previousStepId, null, stepDescription);
+  }
 }
