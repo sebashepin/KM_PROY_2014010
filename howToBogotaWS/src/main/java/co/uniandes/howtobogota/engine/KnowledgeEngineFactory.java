781ebf859299353ea9943f3be46ed7e561ff9c5c
Andrea C <ac.buitrago@bquest.com.co>
2014-11-27 16:08:25 -0500

    Actualizado el KEFactory

--- howToBogotaWS/src/main/java/co/uniandes/howtobogota/engine/KnowledgeEngineFactory.java
+++ howToBogotaWS/src/main/java/co/uniandes/howtobogota/engine/KnowledgeEngineFactory.java
@@ -1,11 +1,13 @@
 package co.uniandes.howtobogota.engine;
 
+import co.uniandes.howtobogota.jenaont.HowToBogotaEngine;
+
 public abstract class KnowledgeEngineFactory {
 	
 	public static final KnowledgeEngine getDefaultKnowledgeEngine()
 	{
 		//TODO return actual engine, fine by the moment though
-		return new PlaceholderKnowledgeEngine();
+		return new HowToBogotaEngine();
 	}
 
 	
