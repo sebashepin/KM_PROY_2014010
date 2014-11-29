0d25d945f1c129ffda1bfee99bcba701913dbc06
Daniel Salamanca <dsdesign@danielsalamanca.com>
2014-11-28 21:55:07 -0500

    arreglos de llamados a rest

--- howToBogota/js/uploadData.js
+++ howToBogota/js/uploadData.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('#preguntar #askQuestionForm').submit(function(){  
+	$( "body" ).load( "respuesta.html" );
        $('#preguntar .printResults').html("<b>Enviando...</b>");
        $.post('rest/GetAnswer', $(this).serialize(), function(data){
             $('#preguntar .printResults').html(data);             
@@ -14,7 +15,7 @@ $(document).ready(function(){
 $('#responderPregunta #createQuestionForm').submit(function(){
 	$('#responderPregunta .printResults').html("<b>Enviando...</b>");
 	
-	$.post('postReceiver.php', $(this).serialize(), function(data){
+	$.post('rest/CreateAndAnswerQuestion', $(this).serialize(), function(data){
             $('#responderPregunta .printResults').html(data);
         }).fail(function() {
 				alert( "Envío fallido." );
