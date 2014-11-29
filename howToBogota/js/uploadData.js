cf649db870ee3723aded118d86dab0b2cf52d205
Daniel Salamanca <dsdesign@danielsalamanca.com>
2014-11-28 23:02:40 -0500

    responderpregutna con dos inputs con value quemados

--- howToBogota/js/uploadData.js
+++ howToBogota/js/uploadData.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('#preguntar #askQuestionForm').submit(function(){  
 	$( "body" ).load( "respuesta.html" );
+		//$.post('postReceiver.php', $(this).serialize(), function(data){
        $('#preguntar .printResults').html("<b>Enviando...</b>");
        $.post('rest/GetAnswer', $(this).serialize(), function(data){
             $('#preguntar .printResults').html(data);             
@@ -22,29 +23,6 @@ $('#responderPregunta #createQuestionForm').submit(function(){
         });
 		return false;
     });	
-	
-
-/*
-$( "#responderPregunta #askQuestionForm" ).submit(function( event ) {
- 
-  // Stop form from submitting normally
-  event.preventDefault();
- 
-  // Get some values from elements on the page:
-  var $form = $( this ),
-    term = $form.find( "input[name='question']" ).val(),
-    url = "http://25.141.65.84:8080/HowToBogota/rest/HowToBogota/GetAnswer";
- 
-  // Send the data using post
-  var posting = $.post( url, { question: term } );
- 
-  // Put the results in a div
-  posting.done(function( data ) {
-    var content = $( data ).find( "#printResults" );
-    $( "#printResults" ).append( content );
-  });
-});
-*/
 
 
 });
