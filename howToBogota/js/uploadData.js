0132205a5ea2cd6b69ff694f0390b30aa42271f3
Daniel Salamanca <dsdesign@danielsalamanca.com>
2014-11-28 22:35:21 -0500

    uploadData.js con llamado a php

--- howToBogota/js/uploadData.js
+++ howToBogota/js/uploadData.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 $(document).ready(function(){
 $('#responderPregunta #createQuestionForm').submit(function(){
 	$('#responderPregunta .printResults').html("<b>Enviando...</b>");
-	
+	//$.post('postReceiver.php', $(this).serialize(), function(data){
 	$.post('rest/CreateAndAnswerQuestion', $(this).serialize(), function(data){
             $('#responderPregunta .printResults').html(data);
         }).fail(function() {
